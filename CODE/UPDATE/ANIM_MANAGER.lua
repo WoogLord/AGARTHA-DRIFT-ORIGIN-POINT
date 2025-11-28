477b42a0fe24587b6389b6f12667c6848ca7b81e
WoogLord <mhnelson428@gmail.com>
2025-11-28 00:03:22 -0500

    AnimClass -> Setup classs properly, now make functions

--- CODE/UPDATE/ANIM_MANAGER.lua
+++ CODE/UPDATE/ANIM_MANAGER.lua
@@ -2,47 +2,51 @@ local AnimClass = {}
 AnimClass.__index = AnimClass
 
 function AnimClass:new(
-          _vanityNames, _frames, _direction, _state, _spriteSheetRow, _animations
+          _vanityNames, _state, _direction, _spriteSheetRow, _frames, _animations
     )
     local tAC = {} -- tAC = tempAnimClass
     setmetatable(tAC, AnimClass)
 
-    tAC.vanityNames, tAC.frames, tAC.direction = _vanityNames, _frames, _direction
-    tAC.state, tAC.spriteSheetRow, tAC.animations = _state, _spriteSheetRow, _animations
+    tAC.vanityNames, tAC.state, tAC.direction = _vanityNames, _state, _direction
+    tAC.spriteSheetRow, tAC.frames, tAC.animations = _spriteSheetRow, _frames, _animations
 
     return tAC
 end
 
 function AnimClass:PrintData()
-    print("\n--==+==-- AnimClass --==+==--"
-        .."\nvanityNames: "..self.vanityNames.."\nframes: "..self.frames.."\ndirection: "..self.direction
-        .."\nstate: "..self.state.."\nspriteSheetRow: "..self.spriteSheetRow.."\nanimations: "..self.animations
-        .."\n"
-    )
+    print("\n--==+==-- AnimClass --==+==--")
+    
+    for i = 1, #self.vanityNames, 1 do 
+        print("\n-- "..i.." --"
+            .."\nvanityNames: "..self.vanityNames[i].."\nstate: "..self.state[i].."\ndirection: "..self.direction[i]
+            .."\nspriteSheetRow: "..self.spriteSheetRow[i].."\nframes: "..self.frames[i].."\nanimations: "..tostring(self.animations[i])
+            .."\n"
+        )
+    end
 end
 
 mainAnimationArray = AnimClass:new(
     { -- _vanityNames
         "IdleUp", "IdleLeft", "IdleDown", "IdleRight"
     }
-    , { -- _frames
-        4,4,4,4
+    , { -- _state
+        "Idle", "Idle", "Idle", "Idle"
     }
     , { -- _direction
         "Up", "Left", "Down", "Right"
     }
-    , { -- _state
-        "Idle", "Idle", "Idle", "Idle"
-    }
     , { -- _spriteSheetRow
         0, 1, 2, 3
+    }
+    , { -- _frames
+        4,4,4,4
     }
     , { -- _animations
         -- loop this later
         {}, {}, {}, {}
     }
 )
--- mainAnimationArray:PrintData()
+mainAnimationArray:PrintData()
 
 function animStateMachine() -- PROBLEM, this is just for the player...
     if player.inBattle then
